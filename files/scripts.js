ddc8c6e37d5adf1c799298054726a1a31a46743a
mazaflame <89023931+mazaflame@users.noreply.github.com>
2022-11-18 16:39:10 -0300

    minor change

---
+++ files/scripts.js
@@ -0,0 +1,38 @@
+function newItem(){
+
+    //javascript
+    //1. Adding a new item to the list of items: 
+       let li = $('<li></li>');
+       let inputValue = $('input').val();
+      let text=document.createTextNode(inputValue);
+      li.append('text');
+    
+    
+       if (inputValue === '') {
+         alert("You must write something!");
+       } else {
+         let list = $('#list');
+         list.append(li);
+       }
+    
+     //2. Crossing out an item from the list of items:
+       function crossOut() {
+             li.toggle("strike");
+         }
+    
+         $on("dblclick",crossOut);
+    
+     //3(i). Adding the delete button "X": 
+       let crossOutButton = $("<crossOutButton></crossOutButton>");
+         crossOutButton.append(document.createTextNode("X"));
+         li.append(crossOutButton);
+    
+         crossOutButton.$("click", deleteListItem);
+     //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+       function deleteListItem(){
+             li.addClass("delete")
+         }
+     // 4. Reordering the items: 
+       $('#list').sortable();
+    
+    }
